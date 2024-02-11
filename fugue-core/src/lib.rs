a9869d90d086e5b186695b1e117c4221a23983d8
Sam Thomas <sam@binarly.io>
2024-02-11 01:26:57 +0000

    expose fugue_state as fugue::state

--- fugue-core/src/lib.rs
+++ fugue-core/src/lib.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "fp")]
 pub use fugue_fp as fp;
 
+#[cfg(feature = "state")]
+pub use fugue_state as state;
+
 pub use fugue_arch as arch;
 pub use fugue_bv as bv;
 pub use fugue_bytes as bytes;
