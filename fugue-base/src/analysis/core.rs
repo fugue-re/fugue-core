f834b9a11c8ee60a3fcc669a168ea52e5cba9885
Sam L. Thomas <s@ghost.sh>
2025-04-18 04:04:54 +0100

    basic analysis pass manager

--- fugue-base/src/analysis/core.rs
+++ fugue-base/src/analysis/core.rs
@@ -1,92 +1,247 @@
-use std::collections::{BTreeSet, VecDeque};
+use std::collections::HashMap;
+
+use thiserror::Error;
 
 use crate::project::Project;
-use crate::storage::StorageProvider;
-use crate::types::Address;
 
-pub struct ControlFlowRecoveryConfig {
-    pub max_blocks: usize,
+pub mod functions;
+
+#[derive(Debug, Error)]
+pub enum AnalysisError {
+    #[error("analysis pass forms a cyclic dependency: {0} -> {1}")]
+    CyclicDependency(String, String),
+    #[error("analysis pass not found: {0}")]
+    PassNotFound(String),
+    #[error("analysis pass failed: {0}")]
+    PassFailed(String, anyhow::Error),
 }
 
-impl Default for ControlFlowRecoveryConfig {
-    fn default() -> Self {
-        ControlFlowRecoveryConfig { max_blocks: 65536 }
-    }
+pub struct AnalysisManager<S = ()> {
+    passes: HashMap<String, Box<dyn AnalysisPass<S>>>,
 }
 
-pub struct ControlFlowRecovery {
-    config: ControlFlowRecoveryConfig,
-    candidates: VecDeque<Address>,
-}
-
-struct FunctionBuilder {
-    candidates: VecDeque<Address>,
-    local_targets: BTreeSet<Address>,
-    global_targets: BTreeSet<Address>,
-}
-
-impl ControlFlowRecovery {
+impl<S> AnalysisManager<S> {
     pub fn new() -> Self {
-        ControlFlowRecovery::new_with(ControlFlowRecoveryConfig::default())
-    }
-
-    pub fn new_with(config: ControlFlowRecoveryConfig) -> Self {
-        ControlFlowRecovery {
-            config,
-            candidates: VecDeque::new(),
+        AnalysisManager {
+            passes: HashMap::new(),
         }
     }
 
-    pub fn add_candidate(&mut self, address: impl Into<Address>) {
-        self.candidates.push_back(address.into());
+    pub fn add_pass(&mut self, name: impl Into<String>, pass: impl AnalysisPass<S> + 'static) {
+        self.passes.insert(name.into(), Box::new(pass));
     }
 
-    pub fn add_candidates(&mut self, addresses: impl IntoIterator<Item = impl Into<Address>>) {
-        self.candidates
-            .extend(addresses.into_iter().map(|addr| addr.into()));
-    }
-
-    pub fn analyse(&mut self, project: &mut Project) {
-        let mut builder = FunctionBuilder::new();
-
-        while let Some(address) = self.candidates.pop_front() {
-            if !project.storage.contains_segment(address) {
-                tracing::trace!("skipping {address}: not mapped");
-            }
-
-            let _f = builder.analyse(project, address);
-
-            todo!()
+    pub fn analyse_with(
+        &mut self,
+        project: &mut Project,
+        pass_name: &str,
+        state: &mut S,
+    ) -> Result<(), AnalysisError> {
+        if let Some(pass) = self.passes.get_mut(pass_name) {
+            pass.analyse_with(project, state)
+        } else {
+            Err(AnalysisError::PassNotFound(pass_name.to_string()))
         }
     }
 }
 
-impl FunctionBuilder {
-    pub fn new() -> Self {
-        FunctionBuilder {
-            candidates: VecDeque::new(),
-            local_targets: BTreeSet::new(),
-            global_targets: BTreeSet::new(),
+impl AnalysisManager {
+    pub fn analyse(&mut self, project: &mut Project, pass_name: &str) -> Result<(), AnalysisError> {
+        self.analyse_with(project, pass_name, &mut Default::default())
+    }
+}
+
+pub trait AnalysisPass<S = ()> {
+    fn analyse(&mut self, #[allow(unused)] project: &mut Project) -> Result<(), AnalysisError> {
+        unimplemented!(
+            "either `AnalysisPass::analyse` or `AnalysisPass::analyse_with` must be implemented"
+        )
+    }
+
+    fn analyse_with(
+        &mut self,
+        project: &mut Project,
+        #[allow(unused)] state: &mut S,
+    ) -> Result<(), AnalysisError> {
+        self.analyse(project)
+    }
+}
+
+impl<S, F> AnalysisPass<S> for F
+where
+    F: FnMut(&mut Project, &mut S) -> Result<(), AnalysisError>,
+{
+    fn analyse_with(&mut self, project: &mut Project, state: &mut S) -> Result<(), AnalysisError> {
+        self(project, state)
+    }
+}
+
+pub trait AnalysisCondition {
+    fn evaluate(&mut self) -> bool;
+}
+
+impl<F> AnalysisCondition for F
+where
+    F: FnMut() -> bool,
+{
+    fn evaluate(&mut self) -> bool {
+        self()
+    }
+}
+
+impl AnalysisCondition for usize {
+    fn evaluate(&mut self) -> bool {
+        if *self > 0 {
+            *self -= 1;
+            true
+        } else {
+            false
         }
     }
+}
 
-    pub fn add_candidate(&mut self, address: impl Into<Address>) {
-        self.candidates.push_back(address.into());
+pub struct AnalysisGroup<S = ()> {
+    passes: Vec<Box<dyn AnalysisPass<S>>>,
+}
+
+impl<S> AnalysisGroup<S> {
+    pub fn new() -> Self {
+        AnalysisGroup { passes: Vec::new() }
     }
 
-    pub fn add_candidates(&mut self, addresses: impl IntoIterator<Item = impl Into<Address>>) {
-        self.candidates
-            .extend(addresses.into_iter().map(|addr| addr.into()));
+    pub fn add_pass(&mut self, pass: impl AnalysisPass<S> + 'static) {
+        self.passes.push(Box::new(pass));
     }
 
-    pub fn clear(&mut self) {
-        self.candidates.clear();
-        self.local_targets.clear();
-        self.global_targets.clear();
+    pub fn add_passes(&mut self, passes: impl IntoIterator<Item = impl AnalysisPass<S> + 'static>) {
+        self.passes.extend(
+            passes
+                .into_iter()
+                .map(|pass| Box::new(pass) as Box<dyn AnalysisPass<S>>),
+        );
     }
+}
 
-    pub fn analyse(&mut self, project: &mut Project, address: impl Into<Address>) {
-        let address = address.into();
-        todo!()
+impl<S> AnalysisPass<S> for AnalysisGroup<S> {
+    fn analyse_with(&mut self, project: &mut Project, state: &mut S) -> Result<(), AnalysisError> {
+        for pass in self.passes.iter_mut() {
+            pass.analyse_with(project, state)?;
+        }
+        Ok(())
+    }
+}
+
+pub struct IteratedAnalysis<S = ()> {
+    pass: Box<dyn AnalysisPass<S>>,
+    condition: Box<dyn AnalysisCondition>,
+}
+
+impl<S> IteratedAnalysis<S> {
+    pub fn new(
+        pass: impl AnalysisPass<S> + 'static,
+        condition: impl AnalysisCondition + 'static,
+    ) -> Self {
+        IteratedAnalysis {
+            pass: Box::new(pass),
+            condition: Box::new(condition),
+        }
+    }
+}
+
+impl<S> AnalysisPass<S> for IteratedAnalysis<S> {
+    fn analyse_with(&mut self, project: &mut Project, state: &mut S) -> Result<(), AnalysisError> {
+        while self.condition.evaluate() {
+            self.pass.analyse_with(project, state)?;
+        }
+        Ok(())
+    }
+}
+
+pub struct ConditionalAnalysis<S = ()> {
+    pass: Box<dyn AnalysisPass<S>>,
+    condition: Box<dyn AnalysisCondition>,
+}
+
+impl<S> ConditionalAnalysis<S> {
+    pub fn new(
+        pass: impl AnalysisPass<S> + 'static,
+        condition: impl AnalysisCondition + 'static,
+    ) -> Self {
+        ConditionalAnalysis {
+            pass: Box::new(pass),
+            condition: Box::new(condition),
+        }
+    }
+}
+
+impl<S> AnalysisPass<S> for ConditionalAnalysis<S> {
+    fn analyse_with(&mut self, project: &mut Project, state: &mut S) -> Result<(), AnalysisError> {
+        if self.condition.evaluate() {
+            self.pass.analyse_with(project, state)?;
+        }
+        Ok(())
+    }
+}
+
+pub struct StatefulAnalysis<S> {
+    pass: Box<dyn AnalysisPass<S>>,
+    state: S,
+}
+
+impl<S> StatefulAnalysis<S> {
+    pub fn new(pass: impl AnalysisPass<S> + 'static, state: S) -> Self {
+        StatefulAnalysis {
+            pass: Box::new(pass),
+            state,
+        }
+    }
+}
+
+impl<S> AnalysisPass for StatefulAnalysis<S> {
+    fn analyse(&mut self, project: &mut Project) -> Result<(), AnalysisError> {
+        self.pass.analyse_with(project, &mut self.state)
+    }
+}
+
+pub type NoState = ();
+
+#[cfg(test)]
+mod test {
+    use crate::storage::InMemoryStorage;
+
+    use super::*;
+
+    #[test]
+    fn test_analysis_passes() -> Result<(), Box<dyn std::error::Error>> {
+        let mut analyses = AnalysisManager::new();
+        let mut project = Project::<InMemoryStorage>::from_file("tests/ls.elf")?;
+
+        analyses.add_pass(
+            "hello-world",
+            |_project: &mut Project, _state: &mut NoState| {
+                println!("Hello, world!");
+                Ok(())
+            },
+        );
+
+        analyses.add_pass(
+            "cond-hello-world",
+            IteratedAnalysis::new(
+                StatefulAnalysis::new(
+                    |_project: &mut Project, state: &mut bool| {
+                        println!("Hello, world; state is {state}!");
+                        *state = !*state;
+                        Ok(())
+                    },
+                    false,
+                ),
+                5,
+            ),
+        );
+
+        analyses.analyse(&mut project, "hello-world")?;
+        analyses.analyse(&mut project, "cond-hello-world")?;
+
+        Ok(())
     }
 }
