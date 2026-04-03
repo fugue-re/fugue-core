a6ed675a2d897a1fede2c663131e22ab84c2ead5
Sam L. Thomas <s@ghost.sh>
2026-04-03 01:39:46 +0100

    simplify bigint features

--- fugue-core/src/lib.rs
+++ fugue-core/src/lib.rs
@@ -1,3 +1,15 @@
+#[cfg(all(
+    feature = "fp",
+    any(
+        feature = "bigint-malachite",
+        feature = "fixed-u64",
+        feature = "fixed-u128",
+    )
+))]
+compile_error!(
+    "the `fp` feature is only compatible with the default `bigint`/`bigint-rug` feature"
+);
+
 pub use fugue_bv as bv;
 #[cfg(feature = "db")]
 pub use fugue_db as db;
